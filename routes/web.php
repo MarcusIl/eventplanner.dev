530d57ce8c8ed80c77ddc99034365b7949c57ae4
Aleksis-t <tukissaleksis@gmail.com>
2023-06-29 18:21:19 +0300

    s

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\EventController;
 use App\Http\Controllers\GuestController;
 use App\Http\Controllers\TaskController;
 use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\RegisterController;
 use Illuminate\Support\Facades\Route;
 
 // Root route
