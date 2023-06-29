76c09874640882794947949f8af67db59ce2b2c5
Aleksis-t <tukissaleksis@gmail.com>
2023-06-29 19:53:47 +0300

    s

--- database/seeders/RolesTableSeeder.php
+++ database/seeders/RolesTableSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Role;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
@@ -16,5 +18,13 @@ class RolesTableSeeder extends Seeder
         ];
 
         DB::table('roles')->insert($roles);
+
+        // Retrieve the "Guest" role
+        $guestRole = Role::where('name', 'Guest')->first();
+
+        // Assign the "Guest" role to all existing users
+        User::all()->each(function ($user) use ($guestRole) {
+            $user->roles()->syncWithoutDetaching([$guestRole->id]);
+        });
     }
 }
