bee2afb6b3f2a000ef1dde7b6fc022619f9c15a8
Aleksis-t <tukissaleksis@gmail.com>
2023-06-29 16:22:09 +0300

    bb

--- routes/web.php
+++ routes/web.php
@@ -25,10 +25,6 @@ Route::post('/events/{event}/budgets', [BudgetController::class, 'create']);
 Route::put('/events/{event}/budgets/{budget}', [BudgetController::class, 'update']);
 Route::delete('/events/{event}/budgets/{budget}', [BudgetController::class, 'delete']);
 
-
-
-Auth::routes();
-// Custom authentication views
 // Authentication Routes
 Route::get('/login', [App\Http\Controllers\Auth\LoginController::class, 'showLoginForm'])->name('login');
 Route::post('/login', [App\Http\Controllers\Auth\LoginController::class, 'login']);
@@ -48,6 +44,7 @@ Route::post('/password/reset', [App\Http\Controllers\Auth\ResetPasswordControlle
 Route::get('/email/verify', [App\Http\Controllers\Auth\VerificationController::class, 'show'])->name('verification.notice');
 Route::get('/email/verify/{id}/{hash}', [App\Http\Controllers\Auth\VerificationController::class, 'verify'])->name('verification.verify');
 Route::post('/email/resend', [App\Http\Controllers\Auth\VerificationController::class, 'resend'])->name('verification.resend');
+
 
 
 
