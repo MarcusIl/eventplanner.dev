f2d847a9194ed3e791a6327a017b658f1b97ecfd
Aleksis-t <tukissaleksis@gmail.com>
2023-06-29 17:21:24 +0300

    yip

--- routes/web.php
+++ routes/web.php
@@ -6,47 +6,53 @@ use App\Http\Controllers\GuestController;
 use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/', [EventController::class, 'index']);
+// Root route
+Route::get('/', function () {
+    return view('welcome');
+})->name('home');
 
 // Event routes
-Route::get('/events', [EventController::class, 'index']); // Add a route to retrieve all events
-Route::post('/events', [EventController::class, 'create']);
-Route::get('/events/{event}', [EventController::class, 'show']);
+Route::get('/events', [EventController::class, 'index'])->name('events.index');
+Route::post('/events', [EventController::class, 'create'])->name('events.create');
+Route::get('/events/{event}', [EventController::class, 'show'])->name('events.show');
 
 // Guest routes
-Route::post('/events/{event}/guests', [GuestController::class, 'create']);
-Route::put('/events/{event}/guests/{guest}', [GuestController::class, 'update']);
-Route::delete('/events/{event}/guests/{guest}', [GuestController::class, 'delete']);
+Route::post('/events/{event}/guests', [GuestController::class, 'create'])->name('guests.create');
+Route::put('/events/{event}/guests/{guest}', [GuestController::class, 'update'])->name('guests.update');
+Route::delete('/events/{event}/guests/{guest}', [GuestController::class, 'delete'])->name('guests.delete');
 
 // Task routes
-Route::post('/events/{event}/tasks', [TaskController::class, 'create']);
-Route::put('/events/{event}/tasks/{task}', [TaskController::class, 'update']);
-Route::delete('/events/{event}/tasks/{task}', [TaskController::class, 'delete']);
+Route::post('/events/{event}/tasks', [TaskController::class, 'create'])->name('tasks.create');
+Route::put('/events/{event}/tasks/{task}', [TaskController::class, 'update'])->name('tasks.update');
+Route::delete('/events/{event}/tasks/{task}', [TaskController::class, 'delete'])->name('tasks.delete');
 
 // Budget routes
-Route::post('/events/{event}/budgets', [BudgetController::class, 'create']);
-Route::put('/events/{event}/budgets/{budget}', [BudgetController::class, 'update']);
-Route::delete('/events/{event}/budgets/{budget}', [BudgetController::class, 'delete']);
+Route::post('/events/{event}/budgets', [BudgetController::class, 'create'])->name('budgets.create');
+Route::put('/events/{event}/budgets/{budget}', [BudgetController::class, 'update'])->name('budgets.update');
+Route::delete('/events/{event}/budgets/{budget}', [BudgetController::class, 'delete'])->name('budgets.delete');
 
 // Authentication Routes
-Route::get('/login', 'LoginController@showLoginForm')->name('login');
-Route::post('/login', 'LoginController@login');
-Route::post('/logout', 'LoginController@logout')->name('logout');
+Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [LoginController::class, 'login']);
+Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
 
 // Registration Routes
-Route::get('/register', 'RegisterController@showRegistrationForm')->name('register');
-Route::post('/register', 'RegisterController@register');
+Route::get('/register', [RegisterController::class, 'showRegistrationForm'])->name('register');
+Route::post('/register', [RegisterController::class, 'register']);
 
 // Password Reset Routes
-Route::get('/password/reset', 'ForgotPasswordController@showLinkRequestForm')->name('password.request');
-Route::post('/password/email', 'ForgotPasswordController@sendResetLinkEmail')->name('password.email');
-Route::get('/password/reset/{token}', 'ResetPasswordController@showResetForm')->name('password.reset');
-Route::post('/password/reset', 'ResetPasswordController@reset');
+Route::get('/password/reset', [ForgotPasswordController::class, 'showLinkRequestForm'])->name('password.request');
+Route::post('/password/email', [ForgotPasswordController::class, 'sendResetLinkEmail'])->name('password.email');
+Route::get('/password/reset/{token}', [ResetPasswordController::class, 'showResetForm'])->name('password.reset');
+Route::post('/password/reset', [ResetPasswordController::class, 'reset']);
 
 // Email Verification Routes
-Route::get('/email/verify', 'VerificationController@show')->name('verification.notice');
-Route::get('/email/verify/{id}/{hash}', 'VerificationController@verify')->name('verification.verify');
-Route::post('/email/resend', 'VerificationController@resend')->name('verification.resend');
+Route::get('/email/verify', [VerificationController::class, 'show'])->name('verification.notice');
+Route::get('/email/verify/{id}/{hash}', [VerificationController::class, 'verify'])->name('verification.verify');
+Route::post('/email/resend', [VerificationController::class, 'resend'])->name('verification.resend');
+
+
+
 
 
 
